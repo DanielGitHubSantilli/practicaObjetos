21b91ab638f91f31119fafd20a3682fc6da20b0b
danielsantilli <danielsantilli1@hotmail.com>
2024-06-22 14:33:13 -0300

    Agregado de parámetro estudios

--- objetos.js
+++ objetos.js
@@ -5,19 +5,20 @@
 //Los argumentos son los valores que se le pasan al constructor.
 //para que un parametro no salga undefined, agrego el signo = "desconocido".
 
-function Persona(nombre,edad,sexo,estadoCivil,signo = "desconocido"){
+function Persona(nombre,edad,sexo,estadoCivil,signo = "desconocido",estudios="desconocido"){
   this.nombre = nombre;
   this.edad = edad; 
   this.sexo = sexo;
   this.estadoCivil = estadoCivil;
-  this.signo = signo
+  this.signo = signo;
+  this.estudios = estudios
 }
 
-let persona = new Persona("pedro", 20, "masculino", "soltero")
+let persona = new Persona("pedro", 20, "masculino", "soltero", "", "Analista de Sistemas")
 console.log(persona);
-let persona1 = new Persona("Daniel", 47, "masculino", "concubino")
+let persona1 = new Persona("Daniel", 47, "masculino", "concubino","","Desarrollador Web")
 console.log(persona1);
-let persona2 = new Persona("Ana", 45, "femenino", "casado","Aries")
+let persona2 = new Persona("Ana", 45, "femenino", "casado","Aries","Docente")
 console.log(persona2);
-let persona3 = new Persona("Marta", 20, "femenino", "soltera","Capricornio")
+let persona3 = new Persona("Marta", 20, "femenino", "soltera","Capricornio","Diseñadora Web Full Stack")
 console.log(persona3);
